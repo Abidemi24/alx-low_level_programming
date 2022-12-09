a7297209991dc5680ea0c7a83b1353ae2b58deac
Abidemi24 <adegunwawunmi@gmail.com>
2022-12-09 08:08:45 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main -prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ *Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)

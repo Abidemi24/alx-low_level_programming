66b8aa314e49923e84fe9677aeb0dd710c8b409c
Abidemi24 <adegunwawunmi@gmail.com>
2022-12-14 13:39:10 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			_putchar (a / 10 + '0');
 			_putchar (a % 10 + '0');
 			_putchar (':');
-			-putchar (b / 10 + '0');
+			_putchar (b / 10 + '0');
 			_putchar (b % 10 + '0');
 			_putchar ('\n');
 		}

061c0a5a68a07e0ab3ff1c737cfcd1b88fd19875
Abidemi24 <adegunwawunmi@gmail.com>
2022-12-14 12:03:03 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,14 @@
  * 0 of number is zero
  * -1 if number is less than zero
  */
-int print_sign(int n)
+ int print_sign(int n)
 {
-	if (n >0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n== 0)
 	{
 		_putchar('0');
 		return (0);
@@ -25,3 +25,38 @@ int print_sign(int n)
 		return (-1);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			

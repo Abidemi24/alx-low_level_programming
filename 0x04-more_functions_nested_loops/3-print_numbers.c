26f06b0414dc6d9c77fefc56fad9e3b508dfbae9
Abidemi24 <adegunwawunmi@gmail.com>
2022-12-16 14:01:44 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  * Return: void
  */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
